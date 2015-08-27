50590d315f6ee641c36e5ed54ce001f216c1a369
Rocky Gray <rocky@videoblocks.com>
2015-08-27 18:01:47 -0400

    make vcard with red background

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,12 @@
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
 	<meta charset="UTF-8">
-	<title>Synopsis Template</title>
+	<title>Rocky Gray Jr.</title>
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width">
+
+    <script src="//use.typekit.net/rur5mbi.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
 
     <!-- bower:css -->
     <link rel='stylesheet' href='vendor/bootstrap.min.css'/>
@@ -17,33 +20,30 @@
     <!-- inject:css -->
     <link rel="stylesheet" href="css/main.css"/>
     <!-- endinject -->
-
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
 
-<div class="container vcard">
+<div class="container">
 
     <div class="row">
-        <div class="col-md-12"><h2 class="name">Rocky Gray Jr.</h2></div>
-        <div class="col-md-12"><span class="title">Software Engineer</span></div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-12">
-            <div class="social-icons">
-                <ul>
-                    <li><i class="fa fa-github"></i>/grocky</li>
-                    <li><i class="fa fa-twitter"></i>/RockyGJr</li>
-                    <li><i class="fa fa-linkedin"></i>/rockygray</li>
-                </ul>
-
+        <div class="vcard col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <div class="row name-title">
+                <div class="col-md-12"><h2 class="name">ROCKY GRAY JR</h2></div>
+                <div class="col-md-12"><span class="title">Software Engineer</span></div>
+            </div>
+            <div class="row social-icons">
+                <div class="col-md-12">
+                    <ul>
+                        <li><i class="fa fa-github"></i><a href="https://github.com/grocky">/grocky</a></li>
+                        <li><i class="fa fa-twitter"></i><a href="https://twitter.com/RockyGJr">/RockyGJr</a></li>
+                        <li><i class="fa fa-linkedin"></i><a href="https://www.linkedin.com/in/rockygray">/rockygray</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row contact-info">
+                <div class="col-md-12">312.857.6259 <span class="separator">/</span> rocky.grayjr@gmail.com <span class="separator">/</span> Gray Designs</div>
             </div>
         </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-12">312.857.6259 / rockyt.grayjr@gmail.com / Gray Designs</div>
     </div>
 
 </div>
