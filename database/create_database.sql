589344d7c8e8e7bccdd071820eeadffac2cf88c4
Rocky Gray <rocky@videoblocks.com>
2015-08-29 11:50:45 -0400

    create_database is just for local dev

---
+++ database/create_database.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS rockygray;
+
+CREATE DATABASE IF NOT EXISTS `rockygray` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+GRANT ALL ON `rockygray`.* to 'php'@'localhost' IDENTIFIED BY 'tlZyNV^Bi61,hDagdTJV';
+GRANT ALL ON `rockygray`.* to 'php'@'127.0.0.1' IDENTIFIED BY 'tlZyNV^Bi61,hDagdTJV';
+GRANT ALL ON `rockygray`.* to 'php_remote'@'%' IDENTIFIED BY 'xkqDQ4LtRUXIB{;UGWCYsMFw3kwE';
+
+FLUSH PRIVILEGES;
+
+DROP DATABASE IF EXISTS rockygray_test ;
+
+CREATE DATABASE IF NOT EXISTS `rockygray_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+GRANT ALL ON `rockygray_test`.* to 'php'@'localhost' IDENTIFIED BY 'tlZyNV^Bi61,hDagdTJV';
+GRANT ALL ON `rockygray_test`.* to 'php'@'127.0.0.1' IDENTIFIED BY 'tlZyNV^Bi61,hDagdTJV';
+GRANT ALL ON `rockygray_test`.* to 'php_remote'@'%' IDENTIFIED BY 'xkqDQ4LtRUXIB{;UGWCYsMFw3kwE';
+
+FLUSH PRIVILEGES;
+
+USE `rockygray`;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO,STRICT_TRANS_TABLES";
+SET time_zone = "-04:00"; /* Note this only sets timezone for the current connection; -04:00 is EDT */
