77ded41167bdddfac51420211b6265a33a2528a3
Rocky Gray <rocky.grayjr@gmail.com>
2016-08-28 13:10:39 -0400

    refactor linkedList classes and add more tests

--- client/js/utils/LinkedList.js
+++ client/js/utils/LinkedList.js
@@ -1,9 +1,30 @@
 
+const NullNode = {
+  getData: () => { throw new Error('Accessing data on an empty list') },
+  getNext: () => { throw new Error('Accessing next on an empty list') },
+  isEmpty: () => true,
+  map: function(fn: func) {
+    return this;
+  },
+  reduce: (fn: func, memo) => memo,
+  reduceRight: (fn: func, memo) => memo
+};
+
 export class Node {
-  constructor(data, next) {
+  constructor(data = null, next: Node) {
+    if (!data && !next) {
+      return Node.NullNode;
+    }
+
     this.data = data;
-    this.next = next;
+    this.next = next || Node.NullNode;
+
+    if (!this.next instanceof Node && this.next !== Node.NullNode) {
+      throw new Error('next must be an instance of Node');
+    }
   }
+
+  static NullNode = NullNode;
 
   getData() {
     return this.data;
@@ -13,19 +34,23 @@ export class Node {
     return this.next;
   }
 
-  isEmpty = false;
+  isEmpty() {
+    return false;
+  }
 
   map(fn: func) {
+    if (this.isEmpty()) {
+    }
     const NodeBuilder = (memo, value) => new Node(fn(value), memo);
-    return this.reduceRight(NodeBuilder, NullNode);
+    return this.reduceRight(NodeBuilder, Node.NullNode);
   }
 
   reduce(fn: func, memo) {
-    return this.next.reduce(fn, fn(memo, this.data))
+    return this.isEmpty() ? memo : this.next.reduce(fn, fn(memo, this.data));
   }
 
   reduceRight(fn: func, memo) {
-    return fn(this.next.reduceRight(fn, memo), this.data);
+    return this.isEmpty() ? memo : fn(this.next.reduceRight(fn, memo), this.data);
   }
 
 }
@@ -33,19 +58,19 @@ export class Node {
 export class ImmutableList {
   constructor(...values) {
     if (values.length === 0) {
-      return NullNode;
+      this.head = Node.NullNode;
+    } else {
+      const data = values[0];
+      const nextValues = [].slice.call(values, 1);
+
+      this.head = new Node(data, new ImmutableList(...nextValues));
     }
-
-    const data = values[0];
-    const nextValues = [].slice.call(values, 1);
-
-    this.head = new Node(data, new ImmutableList(...nextValues));
 
     return this.head;
   }
 
   isEmpty() {
-    return this.head.isEmpty;
+    return this.head.isEmpty();
   }
 
   map(fn: func) {
@@ -60,82 +85,3 @@ export class ImmutableList {
     return this.head.reduceRight(fn, memo);
   }
 }
-
-export const NullNode = {
-  isEmpty: true,
-  get data() {
-    throw new Error('Accessing data on an empty list')
-  },
-  get next() {
-    throw new Error('Accessing next on an empty list')
-  },
-  map: function() { return this; },
-  reduce: (fn, memo) => memo,
-  reduceRight: (fn, memo) => memo
-};
-
-// export class CircularLinkedList {
-//   constructor(root: Node) {
-//     this.head = root;
-//     this.tail = root;
-//     this.tail.setNext(null);
-//
-//     this.iterator = this.head;
-//     this.iteratorPosition = root ? 1 : 0;
-//
-//     this.size = root ? 1 : 0;
-//   }
-//
-//   static of(values: Array, position: number = 1) {
-//     let nodes = values.map(Node.of);
-//     const list = new CircularLinkedList(nodes.shift());
-//     nodes.forEach(list.push);
-//     list.setPosition(position);
-//
-//     return list;
-//   }
-//
-//   static clone(list: CircularLinkedList) {
-//
-//   }
-//
-//   first() {
-//     return this.head.getData();
-//   }
-//
-//   last() {
-//     return this.tail.getData();
-//   }
-//
-//   push(node: Node) {
-//     this.tail.setNext(node);
-//     this.tail = node;
-//     this.tail.setNext(this.head);
-//
-//     this.size++;
-//   }
-//
-//   current() {
-//     return this.iterator.getData();
-//   }
-//
-//   position() {
-//     return this.iteratorPosition;
-//   }
-//
-//   setPosition(position) {
-//     for (const i = 0; i < position; i++) {
-//       this.next();
-//     }
-//   }
-//
-//   next() {
-//     this.iterator = this.iterator.getNext();
-//     this.iteratorPosition++;
-//     return this.iterator.getData();
-//   }
-//
-//   count() {
-//     return this.size;
-//   }
-// }
