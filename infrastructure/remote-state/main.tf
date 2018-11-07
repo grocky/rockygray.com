343dcdfe644abc3c5f009ed27d315fc69eac7896
Rocky Gray <rocky.grayjr@gmail.com>
2018-11-07 16:08:39 -0500

    initialize backend state

---
+++ infrastructure/remote-state/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "grocky-tfstate"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags {
+    Name = "grocky-tfstate"
+    Env = "prod"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name = "tfstate-lock"
+  read_capacity = 1
+  write_capacity = 1
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags {
+    Name = "tfstate-lock"
+    Env = "prod"
+  }
+}
