f68e9506dafa93732c662a58961fc92758bc79c7
Rocky Gray <rocky.grayjr@gmail.com>
2018-11-07 16:15:10 -0500

    use remote s3 backend for state

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,13 +1,21 @@
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket = "grocky-tfstate"
+    dynamodb_table = "tfstate-lock"
+    region = "us-east-1"
+    key = "www.rockygray.com/terraform.tfstate"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
-// Create a variable for our domain name because we'll be using it a lot.
 variable "www_domain_name" {
   default = "www.rockygray.com"
 }
 
-// We'll also need the root domain (also known as zone apex or naked domain).
 variable "root_domain_name" {
   default = "rockygray.com"
 }
