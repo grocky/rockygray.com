d4f67507c4caad54d4613441e4fc73dc6c784145
Rocky Gray <rocky.grayjr@gmail.com>
2019-06-02 18:43:15 -0400

    add redux devtools

--- src/rootRedux.js
+++ src/rootRedux.js
@@ -7,9 +7,12 @@ const rootReducer = combineReducers({
   logos,
 });
 
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 export default function configureStore(preLoadedState) {
   return createStore(
     rootReducer,
     preLoadedState,
+    enhancer,
   );
 }
