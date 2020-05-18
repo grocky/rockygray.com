27e6ea1222f05cf58ec9e67741c8699c0830da56
Rocky Gray <rocky.grayjr@gmail.com>
2020-05-17 23:53:06 -0400

    Use DNS validation for certificate
    the wildcard and apex domains. I received an email for the apex domain from AWS
    and validated it, but the wildcard domain remained pending

--- infrastructure/root-domain.tf
+++ infrastructure/root-domain.tf
@@ -75,7 +75,7 @@ resource "aws_cloudfront_distribution" "root_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = "${aws_acm_certificate.certificate.arn}"
+    acm_certificate_arn = "${aws_acm_certificate_validation.certificate.certificate_arn}"
     ssl_support_method  = "sni-only"
   }
 
--- infrastructure/ssl.tf
+++ infrastructure/ssl.tf
@@ -1,6 +1,6 @@
-resource "aws_acm_certificate" "certificate" {
+resource "aws_acm_certificate" "certificate2" {
   domain_name               = "*.${var.root_domain_name}"
-  validation_method         = "EMAIL"
+  validation_method         = "DNS"
   subject_alternative_names = ["${var.root_domain_name}"]
 
   lifecycle {
@@ -12,4 +12,17 @@ resource "aws_acm_certificate" "certificate" {
     Env         = "prod"
     Application = "www.rockygray.com"
   }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name    = "${aws_acm_certificate.certificate2.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.certificate2.domain_validation_options.0.resource_record_type}"
+  zone_id = "${aws_route53_zone.zone.zone_id}"
+  records = ["${aws_acm_certificate.certificate2.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "certificate" {
+  certificate_arn         = "${aws_acm_certificate.certificate2.arn}"
+  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
 }
